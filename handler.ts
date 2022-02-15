5ae1208c8472838d63eb10d7a3761db38defbe51
Nine@dev <Apisit.a@codemonday.com>
2022-02-16 00:56:18 +0700

    initial lambda for example

---
+++ handler.ts
@@ -0,0 +1,15 @@
+import { APIGatewayEvent } from "aws-lambda";
+
+export const hello = async (event: APIGatewayEvent) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
